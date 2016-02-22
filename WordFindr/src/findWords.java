ca849d77d24cbea4ba9d2fa6d530f6be5f30437f
NoahSelz <noahselz@gmail.com>
2016-02-22 00:27:04 -0600

    Methods set up

--- WordFindr/src/findWords.java
+++ WordFindr/src/findWords.java
@@ -5,13 +5,15 @@ public class findWords {
 //	private static int r;
 //	private static int c;
 //	private static int[] old;
-	public findWords(){
+	public findWords(String[] arry, int n, int m){
 		this.arry = arry;
-		int letlim = m;
+		int letlim = n;
+		int len = m;
 		int r = 0;
 		int c = 0;
 		int[] old;
 	}
+	// blake needs to properly enunciate his needs
 	public void recurse(){
 		
 	}
